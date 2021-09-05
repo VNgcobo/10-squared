4d40badb87e6f7ba7bbf98e6b4e3518d97458972
VNgcobo <85413379+VNgcobo@users.noreply.github.com>
2021-09-05 21:21:42 +0200

    Rebuild of app

--- js/site.js
+++ js/site.js
@@ -1,63 +1,55 @@
 //get values from page
-//control function
-
+//controller function
 function getValues(){
-    //get values from the page(Document Object Model Manipulation - get values and store them in a variable)
+    //get values from the page
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
-   
+
     //convert user input to integers
     startValue = parseInt(startValue);
     endValue = parseInt(endValue);
 
-    //error handling: validate user input
+    //validate user input
     if(Number.isInteger(startValue) && Number.isInteger(endValue)){
         //call generateNumbers
         let numbers = generateNumbers(startValue, endValue);
+
         //call displayNumbers
         displayNumbers(numbers);
-    } else {
-        alert("You must enter numbers");
+    } else{
+        alert("You must enter a number!"); 
     }
-
 }
 
-//generate numbers from the startValue to the endValue
+//generate the numbers
 //logic function
 function generateNumbers(sValue, eValue){
-
     let numbers = [];
 
-    //want to get all numbers from start to end
-    for(let index = sValue; index <= eValue; index++){
-        //this execute in a loop until index = eValue
+    //get values from startValue to endValue
+    for(index = sValue; index <= eValue; index++){
+        //loop from sValue until index = eValue
         numbers.push(index);
     }
-
     return numbers;
 }
 
-//display numbers to the page and mark even numbers bold\
-//display function
+//display the numbers
+//view function
 function displayNumbers(numbers){
-    //loop over array
-    let templateRows = "";
+  //loop over the array
+  let templateRows = "";
 
-    for(let i = 0; i < numbers.length; i++){
+  for(i = 0; i < numbers.length; i++){
+      let className = "even";
 
-        let className = "even";
-        let number = numbers[i];
+      if(number % 2 == 0){
+          className = "even";
+      } else{
+          className = "odd";
+      }
 
-        if(number % 2 == 0){
-            className = "even";
-        }
-        else{
-            className = "odd";
-        }
-
-        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
-
-    }
-
-    document.getElementById("results").innerHTML = templateRows;
+      templateRows += `<tr><td ${className}>${results}</td></tr>`
+  }
+  document.getElementById("results").innerHTML = templateRows;
 }
